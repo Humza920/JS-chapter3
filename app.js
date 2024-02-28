5f417660796e5ec1067a28c4373cd73f1929b804
Humza920 <humza.rehman.779@gmail.com>
2024-02-27 20:11:24 -0800

    CREATED

---
+++ app.js
@@ -0,0 +1,14 @@
+// JS-CHAPTER 3
+
+// Question 1
+
+var age = (15);
+alert("My age is " + age + " years old")
+
+// Question 2
+
+// Question 3
+
+var birthYear = (2006)
+document.write("My birth year is " + birthYear + "Data type of my declared variable is number") 
+
